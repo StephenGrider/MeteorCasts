021387d38009998fb62562b31cea568924925dc5
stephen grider <ste.grider@gmail.com>
2016-04-26 15:42:21 -0700

    add dummy list of images

---
+++ images/client/components/image_detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageDetail = () => {
+  return (
+    <div>Image Detail</div>
+  );
+};
+
+export default ImageDetail;
--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,12 +1,19 @@
 // Create our image list component
 // Import React
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title: 'Pen', link: 'http://dummyimage.com/600x400' },
+  { title: 'Pine Tree', link: 'http://dummyimage.com/600x400' },
+  { title: 'Mug', link: 'http://dummyimage.com/600x400' }
+];
 
 // Create our component
 const ImageList = () => {
   return (
     <ul>
-      <li>Image Detail</li>
+      <ImageDetail />
     </ul>
   );
 };
