b7931e50de34b55ce724d2a550d0306175e3a34f
stephen grider <ste.grider@gmail.com>
2016-04-26 17:48:51 -0700

    added axios

--- images/client/main.js
+++ images/client/main.js
@@ -3,6 +3,7 @@
 // Import the React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
@@ -17,4 +18,6 @@ const App = () => {
 // Render this component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+    .then(response => console.log(response));
 });
