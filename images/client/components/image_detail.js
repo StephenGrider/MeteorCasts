667d7c7971b2e7c5fb5d6fa0440ff0c558b54192
stephen grider <ste.grider@gmail.com>
2016-04-27 17:40:27 -0700

    added imagecsore to image detail

--- images/client/components/image_detail.js
+++ images/client/components/image_detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageScore from './image_score';
 
 const ImageDetail = (props) => {
   return (
@@ -11,6 +12,7 @@ const ImageDetail = (props) => {
           {props.image.title}
         </h4>
         <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </div>
     </li>
   );
