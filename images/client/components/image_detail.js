66633021575c5deef60c8dfdcf4a08325e137336
stephen grider <ste.grider@gmail.com>
2016-04-27 17:30:23 -0700

    added image description

--- images/client/components/image_detail.js
+++ images/client/components/image_detail.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const ImageDetail = (props) => {
-  // props.image => this is the image object
-  // props.image.title
-  // props.image.link
-
   return (
     <li className="media list-group-item">
       <div className="media-left">
@@ -14,6 +10,7 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
       </div>
     </li>
   );
