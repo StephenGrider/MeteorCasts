559631181d97fdb2500c54e92654801844b013ce
stephen grider <ste.grider@gmail.com>
2016-04-29 16:53:45 -0700

    added employee list

---
+++ employees/client/components/employee_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EmployeeList = () => {
+  return (
+    <div>
+      <div className="employee-list">
+        Employee List
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
--- employees/client/main.js
+++ employees/client/main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import EmployeeList from './components/employee_list';
+
 const App = () => {
   return (
-    <div>Hello there!</div>
+    <div>
+      <EmployeeList />
+    </div>
   );
 };
 
