53405750d42c6def0b9665a3f20d7e5d88900f26
stephen grider <ste.grider@gmail.com>
2016-04-28 18:01:26 -0700

    insert records

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,6 +1,8 @@
 // Only executed on the server
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
 import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // Great place to generate data
@@ -10,5 +12,13 @@ Meteor.startup(() => {
   const numberRecords = Employees.find({}).count();
   if (!numberRecords) {
     // Generate some data...
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
   }
 });
