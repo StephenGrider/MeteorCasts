f61f0ecae3768bc364182bd90db194dee40ce4f2
stephen grider <ste.grider@gmail.com>
2016-04-28 17:53:30 -0700

    added count query

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,6 +1,14 @@
 // Only executed on the server
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
 
 Meteor.startup(() => {
   // Great place to generate data
+
+  // Check to see if data exists in the collection
+  // See if the collection has any records
+  const numberRecords = Employees.find({}).count();
+  if (!numberRecords) {
+    // Generate some data...
+  }
 });
