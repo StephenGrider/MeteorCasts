a2cf3400a8a8c717502b1a7d985d61a3dd1850fd
stephen grider <ste.grider@gmail.com>
2016-05-18 16:19:35 -0700

    added logic to validate the url

--- shorten_my_link/imports/collections/links.js
+++ shorten_my_link/imports/collections/links.js
@@ -1,8 +1,10 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
   'links.insert': function(url) {
-    console.log('attempting to save', url);
+    check(url, Match.Where(url => validUrl.isUri(url)));
   }
 });
 
