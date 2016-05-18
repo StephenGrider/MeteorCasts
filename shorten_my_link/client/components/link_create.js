d09d437b61a231b1d2bec839b9d294f1743c35ba
stephen grider <ste.grider@gmail.com>
2016-05-18 14:02:16 -0700

    added submit handler and ref to input

--- shorten_my_link/client/components/link_create.js
+++ shorten_my_link/client/components/link_create.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.refs.link.value);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Link to shorten</label>
-          <input className="form-control" />
+          <input ref="link" className="form-control" />
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
