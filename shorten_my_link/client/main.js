5c6a0d6df65033bcb9b45efd2dc828629a141bf7
stephen grider <ste.grider@gmail.com>
2016-05-18 13:39:02 -0700

    added header

--- shorten_my_link/client/main.js
+++ shorten_my_link/client/main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
+
 const App = () => {
   return (
-    <h1>Hello there!</h1>
+    <div>
+      <Header />
+    </div>
   );
 };
 
