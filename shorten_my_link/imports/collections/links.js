d8d3c729a4a1b3e97918cdd06b57cd231a80e4d8
stephen grider <ste.grider@gmail.com>
2016-05-18 16:38:48 -0700

    generate a token and save it

--- shorten_my_link/imports/collections/links.js
+++ shorten_my_link/imports/collections/links.js
@@ -5,6 +5,10 @@ import { check, Match } from 'meteor/check';
 Meteor.methods({
   'links.insert': function(url) {
     check(url, Match.Where(url => validUrl.isUri(url)));
+
+    // We're ready to save the url
+    const token = Math.random().toString(36).slice(-5);
+    Links.insert({ url, token, clicks: 0 });
   }
 });
 
