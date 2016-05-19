f874384669c1bf3e3d21a444ce2c3ad3ced9ea53
stephen grider <ste.grider@gmail.com>
2016-05-19 15:02:16 -0700

    created app component

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,22 +1,8 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
+import App from './components/app';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
 });
