56964b63b4b7cf485e8affbc714b1800cc83265d
stephen grider <ste.grider@gmail.com>
2016-05-19 15:59:51 -0700

    added bin insert

--- markbin/imports/collections/bins.js
+++ markbin/imports/collections/bins.js
@@ -1,3 +1,14 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+  'bins.insert': function() {
+    return Bins.insert({
+      createdAt: new Date(),
+      content: '',
+      sharedWith: [],
+      ownerId: this.userId
+    });
+  }
+});
+
 export const Bins = new Mongo.Collection('bins');
