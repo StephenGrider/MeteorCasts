22cff9ea244f2e5090eab3e44fd672b03d0a041d
stephen grider <ste.grider@gmail.com>
2016-05-18 18:05:33 -0700

    added onroute middleware

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -9,8 +9,21 @@ Meteor.startup(() => {
   });
 });
 
+// Executed whenever a user visits with a route like
+// 'localhost:3000/abcd'
+function onRoute() {
+  // Take the token out of hte url and try to a find a
+  // matching link in the Links collection
+
+  // If we find a link object, redirect the user to the
+  // long URL
+
+  // If we don't find a link object, send the user
+  // to our normal React app
+}
+
 const middleware = ConnectRoute(function(router) {
-  router.get('/:token', (req) => console.log(req));
+  router.get('/:token', onRoute);
 });
 
 WebApp.connectHandlers.use(middleware);
