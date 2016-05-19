8ecfc684c04943d26c54bb7671f27fe0e5e54571
stephen grider <ste.grider@gmail.com>
2016-05-18 17:58:00 -0700

    added router

--- shorten_my_link/server/main.js
+++ shorten_my_link/server/main.js
@@ -1,8 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function() {
     return Links.find({});
   });
 });
+
+const middleware = ConnectRoute(function(router) {
+  router.get('/:token', (req) => console.log(req));
+});
+
+WebApp.connectHandlers.use(middleware);
