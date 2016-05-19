5f93c4d89288329a96815a9f4b428876803ee7e6
stephen grider <ste.grider@gmail.com>
2016-05-18 17:41:18 -0700

    added link list table

--- shorten_my_link/client/components/link_list.js
+++ shorten_my_link/client/components/link_list.js
@@ -3,9 +3,39 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Links } from '../../imports/collections/links';
 
 class LinkList extends Component {
+  renderRows() {
+    return this.props.links.map(link => {
+      const { url, clicks, token } = link;
+      const shortLink = `http://localhost:3000/${token}`;
+
+      return (
+        <tr key={token}>
+          <td>{url}</td>
+          <td>
+            <a href={shortLink}>{shortLink}</a>
+          </td>
+          <td>
+            {clicks}
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>List of Links to click</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Address</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
     );
   }
 }
--- shorten_my_link/client/main.js
+++ shorten_my_link/client/main.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 import Header from './components/header';
 import LinkCreate from './components/link_create';
 import { Links } from '../imports/collections/links';
+import LinkList from './components/link_list';
 
 const App = () => {
   return (
     <div>
       <Header />
       <LinkCreate />
+      <LinkList />
     </div>
   );
 };
