81a41e9bc39cd4d0b43a41e2e20cc08fe1a3932e
stephen grider <ste.grider@gmail.com>
2016-05-19 16:15:58 -0700

    created bins publication

--- markbin/server/main.js
+++ markbin/server/main.js
@@ -2,5 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  Meteor.publish('bins', function() {
+    return Bins.find({ ownerId: this.userId });
+  });
 });
