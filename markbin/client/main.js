6477cc3bc6b253df6e70bec6659badd76cdd39dc
stephen grider <ste.grider@gmail.com>
2016-05-19 17:02:23 -0700

    added react router

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });
