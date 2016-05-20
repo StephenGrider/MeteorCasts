0894072d9e5c77e39d930ff22bdd728ed4f15280
stephen grider <ste.grider@gmail.com>
2016-05-19 17:33:15 -0700

    creating a bin navigates the user

--- markbin/client/components/header.js
+++ markbin/client/components/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Accounts from './accounts';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 
 class Header extends Component {
   onBinClick(event) {
     event.preventDefault();
 
-    Meteor.call('bins.insert');
+    Meteor.call('bins.insert', (error, binId) => {
+      browserHistory.push(`/bins/${binId}`);
+    });
   }
 
   render() {
