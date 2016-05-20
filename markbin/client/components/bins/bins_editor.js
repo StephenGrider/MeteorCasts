5bcc17d261b69e760cda4ecb44964468bb34ca79
stephen grider <ste.grider@gmail.com>
2016-05-20 14:12:37 -0700

    added bin editor

---
+++ markbin/client/components/bins/bins_editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BinsEditor extends Component {
+  render() {
+    return(
+      <div>Bins Editor</div>
+    );
+  }
+}
+
+export default BinsEditor;
--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
 
 class BinsMain extends Component {
   render() {
-    console.log(this.props.bin);
     return (
-      <div>BinsMain</div>
+      <div>
+        <BinsEditor bin={this.props.bin} />
+      </div>
     );
   }
 }
