59cfff04fd97d8dd505a0be6439ff9d3cc9868ac
stephen grider <ste.grider@gmail.com>
2016-05-20 14:46:48 -0700

    translated html to markdown

--- markbin/client/components/bins/bins_viewer.js
+++ markbin/client/components/bins/bins_viewer.js
@@ -3,9 +3,12 @@ import { markdown } from 'markdown';
 
 class BinsViewer extends Component {
   render() {
+    const rawHTML = markdown.toHTML(this.props.bin.content);
+
     return (
       <div className="col-xs-4">
         <h5>Output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML }}></div>
       </div>
     );
   }
