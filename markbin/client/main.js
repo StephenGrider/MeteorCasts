ea57f85f770b358bf2d6815c7548effa9905624c
stephen grider <ste.grider@gmail.com>
2016-05-19 17:18:41 -0700

    added bin id param

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -11,7 +11,7 @@ const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={BinsList} />
-      <Route path="bins" component={BinsMain} />
+      <Route path="bins/:binId" component={BinsMain} />
     </Route>
   </Router>
 );
