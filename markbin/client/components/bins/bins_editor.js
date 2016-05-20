ff7391efb9051f033e145a6f64929cffb50a62ba
stephen grider <ste.grider@gmail.com>
2016-05-20 14:25:29 -0700

    update bins collection

--- markbin/client/components/bins/bins_editor.js
+++ markbin/client/components/bins/bins_editor.js
@@ -3,11 +3,17 @@ import CodeMirror from 'react-codemirror';
 import 'codemirror/mode/markdown/markdown';
 
 class BinsEditor extends Component {
+  onEditorChange(content) {
+    
+  }
+
   render() {
     return(
       <div className="col-xs-8">
         <h5>Input</h5>
-        <CodeMirror options={{ mode: 'markdown', lineNumbers: true }} />
+        <CodeMirror
+          onChange={this.onEditorChange.bind(this)}
+          options={{ mode: 'markdown', lineNumbers: true }} />
       </div>
     );
   }
--- markbin/imports/collections/bins.js
+++ markbin/imports/collections/bins.js
@@ -12,6 +12,10 @@ Meteor.methods({
 
   'bins.remove': function(bin) {
     return Bins.remove(bin);
+  },
+
+  'bins.update': function(bin, content) {
+    return Bins.update(bin._id, { $set: { content } });
   }
 });
 
