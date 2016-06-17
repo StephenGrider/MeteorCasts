730a06048d1022d2273cc0f6099fdd210c91bfad
stephen grider <ste.grider@gmail.com>
2016-06-17 15:31:45 -0700

    Added bin sharing form

--- markbin/client/components/bins/bins_share.js
+++ markbin/client/components/bins/bins_share.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class BinsShare extends Component {
+  onShareClick() {
+    const email = this.refs.email.value;
+  }
+
   render() {
     return (
       <footer className="bins-share">
-        BinsShare
+        <div className="input-group">
+          <input ref="email" className="form-control" />
+          <div className="input-group-btn">
+            <button
+              onClick={this.onShareClick.bind(this)}
+              className="btn btn-default">
+              Share Bin
+            </button>
+          </div>
+        </div>
       </footer>
     );
   }
