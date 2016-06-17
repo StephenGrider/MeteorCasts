add3b2b5ebef18b4019fc7f73cd2d27347286140
stephen grider <ste.grider@gmail.com>
2016-06-17 15:53:23 -0700

    share subs

--- markbin/client/components/bins/bins_list.js
+++ markbin/client/components/bins/bins_list.js
@@ -38,6 +38,7 @@ class BinsList extends Component {
 
 export default createContainer(() => {
   Meteor.subscribe('bins');
+  Meteor.subscribe('sharedBins');
 
   return { bins: Bins.find({}).fetch() };
 }, BinsList);
--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -22,6 +22,7 @@ class BinsMain extends Component {
 export default createContainer((props) => {
   const { binId } = props.params;
   Meteor.subscribe('bins');
+  Meteor.subscribe('sharedBins');
 
   return { bin: Bins.findOne(binId) };
 }, BinsMain);
